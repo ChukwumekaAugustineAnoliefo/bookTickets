69b3276ddda282e3e9b4ff4b09031d085c4fd04b
ChukwumekaAugustineAnoliefo <chuksanoliefo@gmail.com>
2022-10-02 19:05:26 +0100

    created the ticket tabs

---
+++ lib/widgets/ticket_tabs.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AppTcketsTabs extends StatelessWidget {
+  const AppTcketsTabs({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
